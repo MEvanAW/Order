268d58a51470e349fff2879c4677dc81ec14887c
M. Evan Anindya W <m.evan.aw@gmail.com>
2022-11-06 13:02:49 +0700

    Add item class.

---
+++ OrderApi/Models/Item.cs
@@ -0,0 +1,31 @@
+﻿namespace OrderApi.Models
+{
+    public class Item
+    {
+        /// <summary>
+        /// ID of the item
+        /// </summary>
+        /// <example>1</example>
+        public uint id { get; set; }
+        /// <summary>
+        /// Code of the item
+        /// </summary>
+        /// <example>SGA32</example>
+        public string item_code { get; set; }
+        /// <summary>
+        /// Description of the item
+        /// </summary>
+        /// <example>Samsung Galaxy A32</example>
+        public string description { get; set; }
+        /// <summary>
+        /// Quantity of the item
+        /// </summary>
+        /// <example>1</example>
+        public uint quantity { get; set; }
+        /// <summary>
+        /// ID of the order the item is part of
+        /// </summary>
+        /// <example>1</example>
+        public uint order_id { get; set; }
+    }
+}
--- OrderApi/Models/Order.cs
+++ OrderApi/Models/Order.cs
@@ -1,6 +1,4 @@
-﻿using System.ComponentModel.DataAnnotations;
-
-namespace OrderApi.Models
+﻿namespace OrderApi.Models
 {
     public class Order
     {
