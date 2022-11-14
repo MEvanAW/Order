020d4abdb742ffe78608d159863974687f2c76b4
M. Evan Anindya W <m.evan.aw@gmail.com>
2022-11-14 14:36:00 +0700

    Add register user dummy endpoint.

---
+++ OrderApi/Controllers/UserController.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using OrderApi.DataAccess;
+using OrderApi.Dto;
+using OrderApi.Models;
+using Swashbuckle.AspNetCore.Annotations;
+
+namespace OrderApi.Controllers
+{
+    [Route("users")]
+    [SwaggerTag("Register, login, update, and delete Users")]
+    public class UserController : Controller
+    {
+        private readonly IDataAccessProvider _dataAccessProvider;
+        private readonly string _badBody = "Missing attribute or body format is not recognized.";
+
+        public UserController(IDataAccessProvider dataAccessProvider)
+        {
+            _dataAccessProvider = dataAccessProvider;
+        }
+
+        /// <summary>
+        /// Register a new user
+        /// </summary>
+        /// <remarks>Register a new user.</remarks>
+        /// <param name="userDto">Order to be made.</param>
+        /// <response code="201 ">User created.</response>
+        /// <response code="400">Body format is not recognized.</response>
+        /// <response code="500">Internal server error.</response>
+        [HttpPost]
+        [ProducesResponseType(typeof(string), 201)]
+        [ProducesResponseType(typeof(string), 400)]
+        [ProducesResponseType(500)]
+        [SwaggerOperation(
+            Summary = "Register a new user",
+            Description = "Register a new user.",
+            OperationId = "RegisterUser"
+        )]
+        public IActionResult Register([FromBody, SwaggerRequestBody("The order payload", Required = true)] UserDto userDto)
+        {
+            if (ModelState.IsValid)
+            {
+                return Created("", "Dummy OK Created");
+            }
+            return BadRequest(_badBody);
+        }
+    }
+}
---
+++ OrderApi/Dto/UserDto.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace OrderApi.Dto
+{
+    public class UserDto
+    {
+        [Required]
+        public uint? Age { get; set; }
+        [Required]
+        public string? Email { get; set; }
+        [Required]
+        public string? Password { get; set; }
+        [Required]
+        public string? Username { get; set; }
+    }
+}
